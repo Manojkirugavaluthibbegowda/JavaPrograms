217db0d972aafec0e196a775e3f22a6b9904db1a
Manojkirugavaluthibbegowda <162566766+Manojkirugavaluthibbegowda@users.noreply.github.com>
2024-10-17 23:00:54 +0100

    Initial commit

---
+++ Sum/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("hello world!");
+        int a = 3, b = 4, result;
+        System.out.println(result = a + b);
+    }
+}
