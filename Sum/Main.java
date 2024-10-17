131303485e30b7087ad1e23744e4373ccaf63b5e
Manojkirugavaluthibbegowda <162566766+Manojkirugavaluthibbegowda@users.noreply.github.com>
2024-10-17 23:19:20 +0100

    Added one more variable to the sum

--- Sum/Main.java
+++ Sum/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String args[]) {
         System.out.println("hello world!");
-        int a = 3, b = 4, result;
-        System.out.println(result = a + b);
+        int a = 3, b = 4, c = 10, result;
+        b = 6;
+
+        System.out.println(result = a + b + c);
     }
 }
