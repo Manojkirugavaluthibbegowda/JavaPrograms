b2274b2aeb8c51662087e9fe96eab13036f1b93a
Manojkirugavaluthibbegowda <162566766+Manojkirugavaluthibbegowda@users.noreply.github.com>
2024-10-19 16:34:20 +0100

    Armstrong number check

---
+++ FunctionsMethods/ThreeDigitArmstronNum.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class ThreeDigitArmstronNum {
+    public static void main(String[] args) {
+        System.out.println("Enter the number: ");
+        Scanner in =  new Scanner(System.in);
+        int num = in.nextInt();
+        System.out.println(IsArmstrong(num));
+    }
+    static boolean IsArmstrong(int num) {
+        int n = num;
+        int sum = 0;
+        while(n > 0){
+            int rem = n % 10;
+            sum += rem * rem * rem;
+            n /= 10;
+        }
+        return num == sum;
+    }
+}
